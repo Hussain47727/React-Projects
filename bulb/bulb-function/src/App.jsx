3196de5b2aba63330d2cb91edf71c44671eeb14d
Hussain <hsrar388@gmail.com>
2024-08-20 05:30:59 -0700

    'Bulb-Function'

---
+++ bulb/bulb-function/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function App() {
+  const [isOn, setIsOn] = useState(false); 
+
+  const backgroundColor = isOn ? 'white' : 'darkgray'; 
+  const btnText = isOn ? 'Turn Off' : 'Turn On'; 
+  function startFunction() {
+    setIsOn(!isOn); 
+  }
+
+  return (
+    <div style={{ backgroundColor, display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
+      <button onClick={startFunction}>{btnText}</button>
+    </div>
+  );
+}
+
+export default App;
