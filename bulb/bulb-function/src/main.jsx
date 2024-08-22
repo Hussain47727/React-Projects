0e936eb41431a4f01177df151eecba3050205855
Hussain <hsrar388@gmail.com>
2024-08-22 06:34:52 -0700

    'bulb'

--- bulb/bulb-function/src/main.jsx
+++ bulb/bulb-function/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
+import App from './App.jsx'
 import './index.css'
-import Test from './Test.jsx'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <Test />
-  // </StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
