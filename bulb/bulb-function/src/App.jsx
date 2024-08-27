daa1c8fb4ffe27b23529a518d2804db205f56388
Hussain <hsrar388@gmail.com>
2024-08-27 02:39:46 -0700

    'bulb'

--- bulb/bulb-function/src/App.jsx
+++ bulb/bulb-function/src/App.jsx
@@ -1,32 +1,21 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
-// function App() {
-//   const [isOn, setIsOn] = useState(false); 
+function App() {
+  const [isOn, setIsOn] = useState(false); 
 
-//   const backgroundColor = isOn ? 'white' : 'darkgray'; 
-//   const btnText = isOn ? 'Turn Off' : 'Turn On'; 
-//   function startFunction() {
-//     setIsOn(!isOn); 
-//   }
+  const backgroundColor = isOn ? 'white' : 'darkgray'; 
+  const btnText = isOn ? 'Turn Off' : 'Turn On'; 
+  function startFunction() {
+    setIsOn(!isOn); 
+  }
 
-//   return (
-//     <div style={{ backgroundColor, display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
-//       <button onClick={startFunction}>{btnText}</button>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-import React from 'react'
-import Navbar from 'Component'
-
-export default function App() {
   return (
-    <div>
-      <Navbar title='hello'/>
+    <div style={{ backgroundColor, display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
+      <button onClick={startFunction}>{btnText}</button>
     </div>
-  )
+  );
 }
+
+export default App;
+
 
