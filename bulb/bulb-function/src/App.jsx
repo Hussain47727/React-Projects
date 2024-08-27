5776985f2f4e8e84ea4453f749bae4c3076171ce
Hussain <hsrar388@gmail.com>
2024-08-27 02:35:06 -0700

    'bulb'

--- bulb/bulb-function/src/App.jsx
+++ bulb/bulb-function/src/App.jsx
@@ -19,12 +19,14 @@
 // export default App;
 
 
-i am sorry sir thats way it is the big mistake of this universe and also take 
-take care of mine and such that it is the big mistake of my and you
-five years of old me navigate these properties and beyound the country behind
-the scene and now it is very seamless situation and condition lets suppose
-today i wake up at 9 am i do my breakfast then brush the teeth and some some exercies
-after that i go toward the guloose e aza of hazrat imam hussain and after that
-i become home and fighting with home people such that it is the best condition onabortfor 
-for me ofcourse we are the best at work for the better placement  of scientific confirmc
-condition and that are the main condition
+import React from 'react'
+import Navbar from 'Component'
+
+export default function App() {
+  return (
+    <div>
+      <Navbar title='hello'/>
+    </div>
+  )
+}
+
