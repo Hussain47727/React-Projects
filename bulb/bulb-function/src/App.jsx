1995b57e1b30ae23c7e76df92071060ba46c260a
Hussain <hsrar388@gmail.com>
2024-08-27 02:44:41 -0700

    'bulb'

--- bulb/bulb-function/src/App.jsx
+++ bulb/bulb-function/src/App.jsx
@@ -1,21 +1,23 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-function App() {
-  const [isOn, setIsOn] = useState(false); 
+// function App() {
+//   const [isOn, setIsOn] = useState(false); 
 
-  const backgroundColor = isOn ? 'white' : 'darkgray'; 
-  const btnText = isOn ? 'Turn Off' : 'Turn On'; 
-  function startFunction() {
-    setIsOn(!isOn); 
-  }
+//   const backgroundColor = isOn ? 'white' : 'darkgray'; 
+//   const btnText = isOn ? 'Turn Off' : 'Turn On'; 
+//   function startFunction() {
+//     setIsOn(!isOn); 
+//   }
 
-  return (
-    <div style={{ backgroundColor, display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
-      <button onClick={startFunction}>{btnText}</button>
-    </div>
-  );
-}
+//   return (
+//     <div style={{ backgroundColor, display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
+//       <button onClick={startFunction}>{btnText}</button>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
 
 
+hello iam cristion ronaldo iam a famous footballer recently i open the youtube 
+channel with the name of ur cristion subscribe , shar , comment 
