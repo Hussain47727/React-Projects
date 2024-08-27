a8f9863218b0a126f06a6fad1d43ee38d1fdca3b
Hussain <hsrar388@gmail.com>
2024-08-27 02:47:13 -0700

    'bulb'

--- bulb/bulb-function/src/App.jsx
+++ bulb/bulb-function/src/App.jsx
@@ -1,25 +1,21 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
-// function App() {
-//   const [isOn, setIsOn] = useState(false); 
+function App() {
+  const [isOn, setIsOn] = useState(false); 
 
-//   const backgroundColor = isOn ? 'white' : 'darkgray'; 
-//   const btnText = isOn ? 'Turn Off' : 'Turn On'; 
-//   function startFunction() {
-//     setIsOn(!isOn); 
-//   }
+  const backgroundColor = isOn ? 'white' : 'darkgray'; 
+  const btnText = isOn ? 'Turn Off' : 'Turn On'; 
+  function startFunction() {
+    setIsOn(!isOn); 
+  }
 
-//   return (
-//     <div style={{ backgroundColor, display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
-//       <button onClick={startFunction}>{btnText}</button>
-//     </div>
-//   );
-// }
+  return (
+    <div style={{ backgroundColor, display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
+      <button onClick={startFunction}>{btnText}</button>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
 
 
-hello iam cristion ronaldo iam a famous footballer recently i open the youtube 
-channel with the name of ur cristion subscribe , shar , comment now a days my <SpeechSynthesisUtterancesitua
-</SpeechSynthesisUtterancesitua>
-sutuation is often maintain bed hellow mub fudfifhqwifhw
