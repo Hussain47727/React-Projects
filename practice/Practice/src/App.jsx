8d45346bad26f6ee6bcba37cff9318a66974ef35
Hussain <hsrar388@gmail.com>
2024-08-29 06:43:42 -0700

    'practice'

--- practice/Practice/src/App.jsx
+++ practice/Practice/src/App.jsx
@@ -1,11 +1,10 @@
 import { useEffect, useRef, useState } from 'react'
 import './App.css'
-import Profile from './profile'
 
 const App = () => {
   return (
     <div>
-      <Profile />
+      
     </div>
   )
 }
