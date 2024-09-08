7276798df9218b5363827d634b96cbeb7e8d101a
Hussain <hsrar388@gmail.com>
2024-09-08 10:27:07 -0700

    'react-router'

---
+++ React-router/react-router/src/assets/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
