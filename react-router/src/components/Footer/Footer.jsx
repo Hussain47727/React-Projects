737eb40c04308a0103baacf9613377c9852ff133
Hussain <hsrar388@gmail.com>
2024-09-11 10:35:15 -0700

    'react-router'

---
+++ react-router/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <h1 className='text-blue-900 '>Hello iam footer</h1>
+    </div>
+  )
+}
+
+export default Footer
--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <App />
+  }
+])
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <Routerprovid router = {router} />
   </StrictMode>,
 )
