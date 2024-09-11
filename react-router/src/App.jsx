9b02b29ea8d3ecf1ee7f8cb941c73754aa916043
Hussain <hsrar388@gmail.com>
2024-09-11 10:04:24 -0700

    'react-router'

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
-import { Outlet } from 'react-router-dom'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/Header/Header'
-
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Header />
-    <Outlet />
+   
     </>
   )
 }
