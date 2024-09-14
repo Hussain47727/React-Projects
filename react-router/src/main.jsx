8378c738afe6627585bf0e56e83c47c1662bd14f
Hussain <hsrar388@gmail.com>
2024-09-14 07:04:30 -0700

    'react-router'

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -9,17 +9,13 @@ import {
   Routes,
 } from "react-router-dom";
 
-const router = createBrowserRouter(
-  createsRoutesFromElements(
-  Path : '/',
-  Element : <app></app>
-  )
-)
+
+
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <RouterProvider router={router} />
+   <App/>
   </StrictMode>
 );
